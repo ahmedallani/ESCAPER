5b3adbe604238933b67df7af527e0f75faa290d6
DhiaDhafer <dhiadhaferr@gmail.com>
2020-12-24 17:26:44 +0100

    fix database

---
+++ frontend/src/app/blogs/display-blogs/display-blogs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-blogs',
+  templateUrl: './display-blogs.component.html',
+  styleUrls: ['./display-blogs.component.css']
+})
+export class DisplayBlogsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
