d6c4ac14a446c08f84eeea246bf114a8a2156410
Mohamed-Khalil-Dhaher <dhaherkhalil91@gmail.com>
2020-12-26 10:50:13 +0100

    fix mongoose

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,13 +1,15 @@
-const router =require('express').Router();
-// const authController=require('../controllers/auth.controller')
-
+const router = require("express").Router();
+const authController = require("../db/controllers/auth.controller");
+const userController = require("../db/controllers/user.controller");
+const error = require("../utils/errors.utils");
 
 // const multer =require('multer')
 // const upload=multer()
 
-router.post("api/register",authController.signUp)
-router.post('api/login',authController.signIn)
-router.get('api/logout',authController.logOut)
+router.post("api/register", authController.signUp);
+router.post("api/login", authController.signIn);
+router.get("api/logout", authController.logOut);
 
-
-module.exports=router;
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.userInfo);
+module.exports = router;
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AddBlogComponent } from './blogs/add-blog/add-blog.component';
 import { DisplayBlogsComponent } from './blogs/display-blogs/display-blogs.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [AppComponent, AddBlogComponent, DisplayBlogsComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  // imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule , MatToolbarModule],
+  imports: [BrowserModule, AppRoutingModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
