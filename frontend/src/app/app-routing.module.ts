64f544ed7a643a068583cdf0a25f6d732f452cb1
ahmed-bouhrira <ahmedbouhrira365@gmail.com>
2020-12-28 11:26:47 +0100

    temp

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'test', component: TestComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,9 +6,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AddBlogComponent } from './blogs/add-blog/add-blog.component';
 import { DisplayBlogsComponent } from './blogs/display-blogs/display-blogs.component';
+import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [AppComponent, AddBlogComponent, DisplayBlogsComponent],
+  declarations: [
+    AppComponent,
+    AddBlogComponent,
+    DisplayBlogsComponent,
+    HomeComponent,
+    TestComponent,
+    UserComponent,
+    LoginComponent,
+    SignupComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
